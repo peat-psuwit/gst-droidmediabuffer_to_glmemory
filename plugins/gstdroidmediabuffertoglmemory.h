b6b28e75289136d15ef956423ce50a901f3a86de
Ratchanan Srirattanamet <peathot@hotmail.com>
2019-08-10 15:15:11 +0700

    Complete the copyright notice

--- plugins/gstdroidmediabuffertoglmemory.h
+++ plugins/gstdroidmediabuffertoglmemory.h
@@ -1,5 +1,5 @@
-/* GStreamer
- * Copyright (C) 2019 FIXME <fixme@example.com>
+/* gst-droidmediabuffer_to_glmemory
+ * Copyright (C) 2019 Ratchanan Srirattanamet <peathot@hotmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
